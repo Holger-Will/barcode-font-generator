f8cd98c87d9d33e63e60a21ec598905afcacc953
Holger Will <h.will@klimapartner.de>
2016-09-01 08:17:43 +0200

    add ASCII Symbol to table

--- tools/toMarkdown.js
+++ tools/toMarkdown.js
@@ -1,8 +1,8 @@
 const codes=require("./code_table.json").codes
 console.log(
-`| Code | A | B | C | ASCII | Bars and Spaces | Weights |
-| --- | --- | --- | --- | --- | --- | --- |`
+`| Code | A | B | C | ASCII | Symbol | Bars and Spaces | Weights |
+| --- | --- | --- | --- | --- | --- | --- | --- |`
 )
 codes.forEach(function(item,index){
-  console.log(`| ${item.code} | ${item.A} | ${item.B} | ${item.C} | ${item.ascii} | ${item.bars} | ${item.weights} |`)
+  console.log(`| ${item.code} | ${item.A} | ${item.B} | ${item.C} | ${item.ascii} | ${String.fromCharCode(item.ascii[0])} | ${item.bars} | ${item.weights} |`)
 })
