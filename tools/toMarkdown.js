57c92003feeb9e2860f6687a3da1e431806bbe97
Holger Will <h.will@klimapartner.de>
2016-09-13 13:27:18 +0200

    remove old code

--- tools/toMarkdown.js
+++
@@ -1,8 +0,0 @@
-const codes=require("./code_table.json").codes
-console.log(
-`| Code | A | B | C | ASCII | Symbol | Bars and Spaces | Weights |
-| --- | --- | --- | --- | --- | --- | --- | --- |`
-)
-codes.forEach(function(item,index){
-  console.log(`| ${item.code} | ${item.A} | ${item.B} | ${item.C} | ${item.ascii} | ${String.fromCharCode(item.ascii[0])} | ${item.bars} | ${item.weights} |`)
-})
